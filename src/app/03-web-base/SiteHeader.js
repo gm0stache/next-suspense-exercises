6eb67dad30ffc36fc8471777c69d0717c4acbb48
Gabriel <45315755+gm0stache@users.noreply.github.com>
2025-01-15 22:06:07 +0100

    refactor: simplify solution

--- src/app/03-web-base/SiteHeader.js
+++ src/app/03-web-base/SiteHeader.js
@@ -1,36 +1,39 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
-import { getNavLinks } from '@/helpers/web-base-helpers';
+import { getNavLinks } from "@/helpers/web-base-helpers";
 
 async function SiteHeader() {
-  let navLinks = await getNavLinks();
-
-  // Only show the first 4 links in the header.
-  navLinks = navLinks.slice(0, 4);
-
   return (
     <header className="site-header">
       <Link href="" className="logo">
         WebBase
       </Link>
       <nav>
-        <ol className="header-nav-links">
-          {navLinks.map(
-            ({ slug, label, href, type }) => (
-              <li key={slug}>
-                <Link
-                  href={href}
-                  className={`header-nav-link ${type}`}
-                >
-                  {label}
-                </Link>
-              </li>
-            )
-          )}
-        </ol>
+        <React.Suspense>
+          <Links />
+        </React.Suspense>
       </nav>
     </header>
+  );
+}
+
+async function Links() {
+  let navLinks = await getNavLinks();
+
+  // Only show the first 4 links in the header.
+  navLinks = navLinks.slice(0, 4);
+
+  return (
+    <ol className="header-nav-links">
+      {navLinks.map(({ slug, label, href, type }) => (
+        <li key={slug}>
+          <Link href={href} className={`header-nav-link ${type}`}>
+            {label}
+          </Link>
+        </li>
+      ))}
+    </ol>
   );
 }
 
